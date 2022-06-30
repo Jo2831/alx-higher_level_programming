586de34128aa2c43e2d368eb784b30807bc606f7
Jo2831 <jo2831@github.com>
2022-06-30 06:46:47 -0800

    python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,17 @@
 def print_arg(argv):
     n = len(argv) - 1
     if n == 0:
-        print("{:d} arguments.".format(n))
+        print("{:d} argument.".format(n))
         return
     else:
-        print("{:d} argument:".format(n))
-    i = 1
-    while i <= n:
-        print("{:d}: {:s}".format(i, argv[i]))
-        i += 1
+        if n == 1:
+            print("{:d} argument:".format(n))
+        else:
+            print("{:d} arguments:".format(n))
+        i = 1
+        while i <= n:
+            print("{:d}: {:s}".format(i, argv[i]))
+            i += 1
 
 
 if __name__ == "__main__":
