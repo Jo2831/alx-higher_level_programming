8a4ee5254e92420fd890c08b13f46d668d32d520
Jo2831 <jo2831@github.com>
2022-07-02 12:19:05 -0800

    fix_task

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    largest = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > largest:
+            largest = my_list[i]
+    return (largest)
