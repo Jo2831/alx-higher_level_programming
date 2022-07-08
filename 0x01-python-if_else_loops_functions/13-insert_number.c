d6027afd15ab84d33cce6d49ac5bd75e0685650d
Jo2831 <jo2831@github.com>
2022-07-08 06:36:23 -0800

    upgrade

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,74 +1,45 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
+#include<stdlib.h>
 
 /**
- * print_listint - prints all elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of nodes
- */
-size_t print_listint(const listint_t *h)
+* insert_node - inserts  node in a sorted singly linked list
+* @head: head of the linked list.
+* @number: data in the new node.
+* Return: address of the new node.
+*/
+
+listint_t *insert_node(listint_t **head, int number)
 {
-	const listint_t *current;
-	unsigned int n; /* number of nodes */
+	listint_t *newNode;
+	listint_t *first;
 
-	current = h;
-	n = 0;
-	while (current != NULL)
-	{
-		printf("%i\n", current->n);
-		current = current->next;
-		n++;
-	}
+	first = *head;
 
-	return (n);
-}
-
-/**
- * add_nodeint_end - adds a new node at the end of a listint_t list
- * @head: pointer to pointer of first node of listint_t list
- * @n: integer to be included in new node
- * Return: address of the new element or NULL if it fails
- */
-listint_t *add_nodeint_end(listint_t **head, const int n)
-{
-	listint_t *new;
-	listint_t *current;
-
-	current = *head;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	newNode = malloc(sizeof(listint_t));
+	if (newNode == NULL)
 		return (NULL);
 
-	new->n = n;
-	new->next = NULL;
+	newNode->n = number;
+	newNode->next = NULL;
 
-	if (*head == NULL)
-		*head = new;
-	else
+	if (*head == NULL || first->n > newNode->n)
 	{
-		while (current->next != NULL)
-			current = current->next;
-		current->next = new;
+		newNode->next = *head;
+		*head = newNode;
+		return (newNode);
 	}
-
-	return (new);
-}
-
-/**
- * free_listint - frees a listint_t list
- * @head: pointer to list to be freed
- * Return: void
- */
-void free_listint(listint_t *head)
-{
-	listint_t *current;
-
-	while (head != NULL)
+	while (first->next != NULL)
 	{
-		current = head;
-		head = head->next;
-		free(current);
+		if ((first->next->n > newNode->n && first->n < newNode->n)
+			|| newNode->n == first->n)
+		{
+			newNode->next = first->next;
+			first->next = newNode;
+			return (newNode);
+		}
+		first = first->next;
 	}
+	newNode->next = first->next;
+	first->next = newNode;
+	return (newNode);
 }
